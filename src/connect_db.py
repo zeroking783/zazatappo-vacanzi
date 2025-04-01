e0ce94df888289e1209eb7be420d02baf18b4424
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-04-01 11:19:57 +0300

    create connect db

---
+++ src/connect_db.py
@@ -0,0 +1,26 @@
+import psycopg2 
+from logger import logger
+import sys
+
+def connect_database(database_secrets):
+    try:
+        logger.debug(f"Пробую подключиться к базе данных")
+        conn = psycopg2.connect(
+            dbname=database_secrets["dbname"],
+            user=database_secrets["user"],
+            password=database_secrets["password"],
+            host=database_secrets["host"],
+            port=database_secrets["port"]
+        )
+    except Exception as e:
+        logger.error(f"Безуспешное подключение к базе данных: {e}")
+        sys.exit(1)
+
+    try:
+        logger.debug(f"Создаю объект база данных cursor")
+        cur = conn.cursor()
+    except Exception as e:
+        logger.error(f"Ошибка создания объекта cursor: {e}")
+        sys.exit(1)
+
+    return conn, cur
