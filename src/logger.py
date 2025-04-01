b9c4a176b63f60b7ed243ae3160c4ee28b866f6d
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-04-01 11:20:08 +0300

    logger

---
+++ src/logger.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from colorama import init, Fore, Style
+
+init()
+
+logger = logging.getLogger('parse_vacancies')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        'DEBUG': Fore.CYAN,
+        'INFO': Fore.GREEN,
+        'WARNING': Fore.YELLOW,
+        'ERROR': Fore.RED,
+        'CRITICAL': Fore.RED + Style.BRIGHT
+    }
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelname, Fore.WHITE)
+        message = super().format(record)
+        return f"{color}{message}{Style.RESET_ALL}"
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(ColoredFormatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+log_file = 'app.log'
+file_handler = logging.FileHandler(log_file)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
