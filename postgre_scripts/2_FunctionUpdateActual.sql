d8c844f546d6c9824f05e78515b55cecaaa4c39a
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-04-03 19:05:10 +0300

    add function if not exists in postgre

--- postgre_scripts/2_FunctionUpdateActual.sql
+++ postgre_scripts/2_FunctionUpdateActual.sql
@@ -11,3 +11,20 @@ CREATE TRIGGER trigger_deactive_old_vacancy
 BEFORE INSERT ON vacancies
 FOR EACH ROW
 EXECUTE FUNCTION deactive_old_vacancy();
+
+CREATE OR REPLACE FUNCTION insert_if_not_exists()
+RETURNS TRIGGER AS $$
+BEGIN 
+    IF EXISTS (
+        SELECT 1 FROM vacancies WHERE link_num = NEW.link_num AND name = NEW.name AND description = NEW.description
+    ) THEN
+        RETURN NULL;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_insert_if_not_exists
+BEFORE INSERT ON vacancies
+FOR EACH ROW
+EXECUTE FUNCTION insert_if_not_exists();
