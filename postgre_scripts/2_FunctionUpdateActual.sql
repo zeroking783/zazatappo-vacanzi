4474a2666fcf2d334b563cd79621a8cb51a3ddd9
zeroking783 <bakanov.vladimir2016@gmail.com>
2025-04-04 14:41:27 +0300

    combined functions

--- postgre_scripts/2_FunctionUpdateActual.sql
+++ postgre_scripts/2_FunctionUpdateActual.sql
@@ -1,30 +1,21 @@
-CREATE OR REPLACE FUNCTION deactive_old_vacancy()
-RETURNS TRIGGER AS $$
-BEGIN 
-    UPDATE vacancies SET actual WHERE link_num = NEW.link_num AND actual = TRUE;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_deactive_old_vacancy
-BEFORE INSERT ON vacancies
-FOR EACH ROW
-EXECUTE FUNCTION deactive_old_vacancy();
-
-CREATE OR REPLACE FUNCTION insert_if_not_exists()
+CREATE OR REPLACE FUNCTION handle_vacancy_insert()
 RETURNS TRIGGER AS $$
 BEGIN 
     IF EXISTS (
-        SELECT 1 FROM vacancies WHERE link_num = NEW.link_num AND name = NEW.name AND description = NEW.description
+        SELECT 1 FROM vacancies 
+        WHERE link_num = NEW.link_num AND name = NEW.name AND description = NEW.description
     ) THEN
         RETURN NULL;
     END IF;
+
+    UPDATE vacancies SET actual = FALSE 
+    WHERE link_num = NEW.link_num AND actual = TRUE;
+
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trigger_insert_if_not_exists
+CREATE TRIGGER trigger_handle_vacancy_insert
 BEFORE INSERT ON vacancies
 FOR EACH ROW
-EXECUTE FUNCTION insert_if_not_exists();
+EXECUTE FUNCTION handle_vacancy_insert();
